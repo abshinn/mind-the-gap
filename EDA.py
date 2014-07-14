52885ae3db7afb03880690b4ef7be2ed307e2d6f
Adam Shinn <adam.b.shinn@gmail.com>
2014-07-14 07:37:37 -0700

    update EDA

--- EDA.py
+++ EDA.py
@@ -1,12 +1,12 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[94]:
 
 get_ipython().magic(u'pylab inline')
 
 
-# In[27]:
+# In[95]:
 
 import pandas as pd
 import numpy as np
@@ -15,31 +15,50 @@ rcParams["figure.figsize"] = 18, 8
 np.set_printoptions(precision=4)
 
 
-# In[49]:
+# ### Read in project data
+
+# In[96]:
 
 df = pd.read_csv("data/opendata_projects.csv", 
                  true_values="t", false_values="f",
                  parse_dates = ["date_posted", "date_completed", "date_expiration"])
 
 
-# In[38]:
+# In[97]:
 
-df.columns
-
-
-# In[39]:
-
-df._schoolid.value_counts().hist(bins=30, log=np.log10)
-plt.title("school projects");
+for column in df.columns:
+    print column
 
 
-# In[40]:
+# ### Histogram: Projects per school [2002 - 2013]
+
+# In[113]:
+
+df._schoolid.value_counts().hist(bins=40, log=np.log10)
+plt.title("Projects / school Histo");
+
+
+# ### How many one-project schools are there?
+
+# In[114]:
 
 one_proj_schools = (df._schoolid.value_counts() == 1).sum()
 uniq_schools = len(df._schoolid.value_counts())
 
-print "One project schools / Total amount of schools\n"
+print "[One project schools / Total amount of schools]\n"
 print "{} / {} -- {:.2f}".format(one_proj_schools, uniq_schools, np.float(one_proj_schools)/uniq_schools)
+
+# print "\n"
+
+# print "[Number of one-project schools that were unsuccessful]\n"
+# one_proj_df = df.loc[(df._schoolid.value_counts() == 1).index]
+# one_proj_expired = len(one_proj_df.funding_status == "expired")
+# print "{} / {} -- {:.2f}".format(one_proj_expired, len(one_proj_df), np.float(one_proj_expired)/len(one_proj_df))
+
+
+# In[101]:
+
+df.funding_status.value_counts()
 
 
 # In[67]:
@@ -49,20 +68,20 @@ df["year_posted"] = year_posted
 years_active = df.groupby("_schoolid").year_posted.nunique()
 projs_per_school = df.groupby("_schoolid")._projectid.nunique()
 
-
-# In[77]:
-
-# years_active.hist(bins=13)
-year_posted.hist(bins=13)
-
-
-# In[81]:
-
 projs_per_year = projs_per_school/years_active.astype(np.float)
 
 
-# In[87]:
+# ### Projects/year per school Histogram
+
+# In[112]:
 
 projs_per_year.hist(bins=40, log=np.log10)
-plt.title("Projects/year")
+plt.title("Projects per year per school Histo");
+
+
+# In[111]:
+
+# years_active.hist(bins=13)
+year_posted.hist(bins=13)
+plt.title("Projects by year");
 
