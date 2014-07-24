cb0b3dba3b39a550c69cfb728d5f08b5003b4f21
Adam Shinn <adam.b.shinn@gmail.com>
2014-07-24 14:58:28 -0700

    add function to grab all districts in the US

--- code/get_census.py
+++ code/get_census.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python2.7 -B
 """get California census by district"""
 
+import os
 import pandas as pd
+import pdb
 
 
-def districts(LEA_id, columns=None, drop_columns=[]):
+def districts(lea_ids=[], columns=[], drop_columns=[], filename=""):
     """
-    INPUT: pandas series of local education agency ids
+    INPUT: pandas series of local education agency ids (optional)
     OUTPUT: pandas dataframe of census data with index as given series of LEA ids
 
     grab California census-by-district data
     """
     print "[grab California census data...]"
 
-    censusdf = pd.read_csv("../data/district/SDDS_School_Districts_California_Jul-17-2014.csv", na_values=['null'])
+    if filename:
+        print "[grabbing census data from: {}]".format(filename)
+    else:
+        print "[grab California census data...]"
+        filename = "../data/district/SDDS_School_Districts_California_Jul-17-2014.csv"
+
+    censusdf = pd.read_csv(filename, na_values=['null'])
     censusdf.index = censusdf.pop("NCES ID")
 
     if columns:
@@ -22,4 +30,24 @@ def districts(LEA_id, columns=None, drop_columns=[]):
     if drop_columns:
         censusdf = censusdf.drop(drop_columns)
 
-    return censusdf.loc[LEA_id]
+    if type(lea_ids) == pd.core.series.Series:
+        return censusdf.loc[LEA_id]
+    else:
+        return censusdf
+
+
+def all_districts(columns=[]):
+    census_path = "../data/census/"
+    csvs = os.listdir(census_path)
+
+    state_dfs = []
+    for csv in csvs:
+        print csv
+        state_dfs.append(districts(filename=(census_path + csv)))
+
+    states = pd.concat(state_dfs, axis=0)
+    pdb.set_trace()
+
+
+if __name__ == "__main__":
+    all_districts()
