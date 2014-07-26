0e13b0c5b6db35ed60a858f91bd9ae6abe2340cb
Adam Shinn <adam.b.shinn@gmail.com>
2014-07-26 01:10:54 -0700

    add rough code that writes most active districts to geojson

--- code/district.py
+++ code/district.py
@@ -10,6 +10,7 @@ import get_donorschoose
 import get_nces
 import get_census
 import pickle
+import geojson
 
 
 def district_similarity(pkle=False):
@@ -21,12 +22,12 @@ def district_similarity(pkle=False):
 
     census = get_census.all_states()
 
-    columns = ["TOTALREV", "TFEDREV", "TSTREV", "TLOCREV", "TOTALEXP", "TCURSSVC", "TCAPOUT", "HR1", "HE1", "HE2"]
+    columns = ["STNAME", "TOTALREV", "TFEDREV", "TSTREV", "TLOCREV", "TOTALEXP", "TCURSSVC", "TCAPOUT", "HR1", "HE1", "HE2"]
     nces = get_nces.districts(columns=columns, nonneg=True)
 
     ddf = pd.concat([census, nces.loc[census.index]], axis=1)
 
-    sim = similarity.Similarity(ddf, ref_columns=["District Name", "State"])
+    sim = similarity.Similarity(ddf, ref_columns=["District Name", "State", "STNAME"])
 
     if pkle:
         with open("district_similarity.pkle", "wb") as p:
@@ -41,23 +42,10 @@ def potential_districts():
     OUTPUT:
     """
 
-#     schools = get_donorschoose.schools(year=2011)
-#     schools = pd.concat([ schools, get_nces.schools(schools.index, columns=["LEAID", "FTE", "MEMBER", "ST_ratio", "TOTFRL"]) ], axis=1)
-# 
-#     dcdistricts = schools.groupby("LEAID").agg({"students_reached": np.sum,
-#                                                 "projects": np.sum,
-#                                                 "percent_funded": np.mean,
-#                                                 "total_donations": np.sum,
-#                                                 "high poverty": np.sum,
-#                                                 "highest poverty": np.sum,
-#                                                 "low poverty": np.sum,
-#                                                 "FTE": np.mean,
-#                                                 "TOTFRL": np.sum,
-#                                                 "MEMBER": np.sum,
-#                                                 "ST_ratio": np.mean,
-#                                                }).sort("projects", ascending=False)
-# 
-    dcdisticts = get_donorschoose.districts(year=2011)
+    dc_districts = get_donorschoose.districts(year=2011)
+
+    all_dc = dc_districts.index
+    most_active = dc_districts.projects[dc_districts.projects >= 100].index
     # pseudo
     # reduce dcdistricts down to most active
     # compile list of potential districts
@@ -65,10 +53,23 @@ def potential_districts():
     # compute activity metric
     # save into csv
 
-    return dcdistricts
+    return dc_districts
        
 
 if __name__ == "__main__":
-#     district_similarity(pkle=True) 
-    potential_districts()
-    pdb.set_trace()
+    sim = district_similarity() 
+    ddf = potential_districts()
+    most_active = ddf.projects[ddf.projects >= 100].index
+    all_dc = ddf.index
+    ma = ddf.loc[most_active]
+    names = sim.data[["District Name", "STNAME"]].loc[ma.index]
+    ma = pd.concat([ma, names], axis=1)
+    ma = ma.dropna()
+    features = []
+    for leaid in ma.index.values.astype(np.int):
+        point = geojson.Point((ma.loc[leaid].longitude, ma.loc[leaid].latitude))
+        properties = {"name" : ma["District Name"].loc[leaid], "state": ma["STNAME"].loc[leaid]}
+        features.append(geojson.Feature(geometry=point, properties=properties))#, id=leaid))
+    collection = geojson.FeatureCollection(features) 
+    with open("test.json", "w") as geo:
+        geo.write(geojson.dumps(collection))
